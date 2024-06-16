4276d1acfea9a46538f8dc4f508460e9524f719f
AnujShukla760 <shuklaanuj312@gmail.com>
2024-06-16 17:43:49 +0530

    java coding

---
+++ start/src/main/java1/src/main/java/org/example/sumofnumbers.java
@@ -0,0 +1,13 @@
+package org.example;
+public class sumofnumbers{
+
+    public static void main(String[] args) {
+        int n1=23; int n2=21; int sum;
+            sum=n1+n2;
+            system.out.println("the sum is "+sum);
+
+
+        }
+
+        }
+
