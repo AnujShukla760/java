0241330cb4c0df96422b5d491f14c26321ff0910
AnujShukla760 <shuklaanuj312@gmail.com>
2024-06-21 17:56:26 +0530

    sql connection

--- start/src/main/java1/src/main/java/org/example/dbconnection/DBConnection.java
+++ start/src/main/java1/src/main/java/org/example/dbconnection/DBConnection.java
@@ -6,11 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public Connection getConnection() throws SQLException {
-        String url = "jdbc:h2:mem:Test"; //h2 database
-        String user = "aa"; // Database username
-        String password = "";  //Database password
+        String url="jdbc:mysql://localhost:3306/ems";
+        String username="root";
+        String password="root";
 
-        return DriverManager.getConnection(url, user, password);
+
+        return DriverManager.getConnection(url, username, password);
 
     }
 }
--- start/src/main/java1/src/main/java/org/example/dbconnection/MainClass.java
+++ start/src/main/java1/src/main/java/org/example/dbconnection/MainClass.java
@@ -1,39 +1,76 @@
 package org.example.dbconnection;
 
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
+
+
+
 
 public class MainClass {
 
-    public static void main(String args[]){
+
+
+    public static void main(String args[]) {
+        String Age;
+        String rollno;
+        String address;
+        String name;
+
         DBConnection test = new DBConnection();
+        Student student = new Student();
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter the Student Age");
+        Age = scanner.nextLine();
+        student.AGE = Integer.parseInt(Age);
+        System.out.println(Age);
+
+
+
+
+//        System.out.println("Enter the Student Age");
+//        rollno = scanner.nextLine();
+//        student.ROLLNO = Integer.parseInt(Age);
+//        System.out.println(Age);
+
+
+//        System.out.println("Enter the Student address");
+//        address = scanner.nextLine();
+//        student.ADDRESS = String.valueOf(address);
+//        System.out.println(address);
+
+
+//        System.out.println("Enter the Student name");
+//        name = scanner.nextLine();
+//        student.NAME = String.valueOf(name);
+//
+//        System.out.println(name);
+
 
         try (Connection connection = test.getConnection()) {
-            Statement statement = connection.createStatement();
-            String createTableQuery = "CREATE TABLE If NOT EXISTS testdb ("
-                    + "id INT PRIMARY KEY,"
-                    + "NAME VARCHAR(255),"
-                    + "TYPEOFEMPLOYEE VARCHAR(255),"
-                    + "SALARY DECIMAL,"
-                    + "ADDRESS VARCHAR(255))";
-            statement.execute(createTableQuery);
-            System.out.println("Table created succesfully,");
-            String query = "INSERT INTO tstdb(id,NAME,TYPEOFEMPLOYEE,SALARY,ADDRESS)"
-                    + "values('" + 12 + "','" + "Test" + "','" + "partTime" + "','" + 123 + "','" + "new"
+            String createQuery="CREATE TABLE student ( age int, rollNumber int)";
+            Statement statements=connection.createStatement();
+            statements.execute(createQuery);
+            String query="INSERT INTO student ( age, rollNumber)"
+                    + "values('" + student.AGE + "','" + student.ROLLNO + "','" + "student.rollNumber"
                     + "')";
-            statement.executeUpdate(query);
-            String querySelect = "SELECT * from testdb where id = 13 ";
-            ResultSet rs = statement.executeQuery(querySelect);
-            if (rs.next()) {
-                System.out.println("Name:" + rs.getString("name"));
-            } else {
-                System.out.println("No data found for id = 13");
-            }
+
+            statements.executeUpdate(query);
+            System.out.println("Successfully connected to the H2 database.");
+
+            String selectQuery="Select * from student";
+            ResultSet result= statements.executeQuery(selectQuery);
+            System.out.println(result.getRow());
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            System.out.println("Failed to connect to the H2 database..");
         }
-        System.out.println("I am testing");
-    }
-}
+
+
+    }}
