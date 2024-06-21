434a7e847a50226d8737acc90b91d172f666e75a
AnujShukla760 <shuklaanuj312@gmail.com>
2024-06-21 22:53:09 +0530

    db connection1

--- start/src/main/java1/src/main/java/org/example/DBConnection1/DBConnection1.java
+++ start/src/main/java1/src/main/java/org/example/DBConnection1/DBConnection1.java
@@ -1,17 +1,17 @@
-package org.example.dbconnection;
+package org.example.DBConnection1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-    public Connection getConnection() throws SQLException {
+public class DBConnection1 {
+
+    public Connection db1() throws SQLException{
         String url="jdbc:mysql://localhost:3306/ems";
         String username="root";
         String password="root";
 
-
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(url,username,password);
 
     }
 }
